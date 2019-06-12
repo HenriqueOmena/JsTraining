ee407699b827f1970dc92d87c6e78b5f7c34c44b
Henrique Omena <88lmao@gmail.com>
2019-06-12 06:49:20 +0100

    prepare front end to receive data from API github

--- Rocketseat/es6/src/main.js
+++ Rocketseat/es6/src/main.js
@@ -1,4 +1,60 @@
-import axios from 'axios'
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list')
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepitory(event)
+    }
+    addRepitory(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: "Henrique Omena",
+            description: "Working hard!",
+            avatar_url: "https://avatars3.githubusercontent.com/u/35804326?s=460&v=4",
+            html_url: "https://api.github.com/users/henriqueomena"
+        })
+
+        this.render()
+        console.log(this.repositories);
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', 'blank');
+            linkEl.appendChild(document.createTextNode('Acessar'))
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl)
+
+        })
+    }
+}
+
+new App();
+
+
+/* import axios from 'axios'
 
 class Api {
     static async getUserInfo(username) {
@@ -11,7 +67,7 @@ class Api {
     }
 }
 
-Api.getUserInfo('henriqueomena')
+Api.getUserInfo('henriqueomena') */
 
 
 /* const minhaPromise = () => new Promise((resolve, reject) => {

b979df870b70fb66a6ea73423811545dd6256d44
Henrique Omena <88lmao@gmail.com>
2019-06-15 09:36:32 +0100

    Start a simple todoList

---
+++ Rocketseat/jstodo/index.js
@@ -0,0 +1,13 @@
+var listEl = document.querySelector('#app ul');
+var inputEl = document.querySelector('#app input');
+var btnEl = document.querySelector('#app button');
+
+var todos = [
+    'Fazer Café',
+    'Estudar JavaScript',
+    'Acessar comunidade de Devs'
+];
+
+const renderTodos = todos.map((el, inde) => {
+    console.log(el);
+})

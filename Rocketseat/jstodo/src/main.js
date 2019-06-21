8b22baedecf790fa5e114dd3cd971f3549748609
Henrique Omena <88lmao@gmail.com>
2019-06-21 23:03:50 +0100

    a simple todo list with only js

--- Rocketseat/jstodo/src/main.js
+++ Rocketseat/jstodo/src/main.js
@@ -1,6 +1,3 @@
-
-
-
 var listEl = document.querySelector('#app ul');
 var inputEl = document.querySelector('#app input');
 var btnEl = document.querySelector('#app button');
@@ -11,6 +8,32 @@ var todos = [
     'Acessar comunidade de Devs'
 ];
 
-const renderTodos = todos.map((el, inde) => {
-    console.log(el);
-})
+const renderTodos = () => {
+        listEl.innerHTML = '';
+
+        for (todo of todos) {
+            var todoElement = document.createElement('li');
+            var todoText = document.createTextNode(todo);
+
+            todoElement.appendChild(todoText);
+            listEl.appendChild(todoElement);
+        }
+}
+
+renderTodos();
+
+
+// const renderTodos = todos.map((el, inde) => {
+//     //console.log(el);
+// })
+
+const addTodo = () =>{
+    var todoText = inputEl.value;
+
+    todos.push(todoText);
+
+    inputEl.value = '';
+    renderTodos();
+}
+
+btnEl.onclick = addTodo;

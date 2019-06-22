36bc3c809232909bd175bbdae95fc734212e31c2
Henrique Omena <88lmao@gmail.com>
2019-06-22 01:38:39 +0100

    Delete and show messages erros if empty

--- Rocketseat/jstodo/src/main.js
+++ Rocketseat/jstodo/src/main.js
@@ -1,34 +1,57 @@
-var listEl = document.querySelector('#app ul');
-var inputEl = document.querySelector('#app input');
-var btnEl = document.querySelector('#app button');
+const listEl = document.querySelector('#app ul');
+const inputEl = document.querySelector('#app input');
+const btnEl = document.querySelector('#app button');
+const infoEl = document.querySelector('#app div.info')
 
-var todos = [
+const todos = [
     'Fazer Café',
     'Estudar JavaScript',
     'Acessar comunidade de Devs'
 ];
 
+inputEl.addEventListener('keyup', event => event.keyCode === 13 ? btnEl.click() : '')
+
+
+
 const renderTodos = () => {
         listEl.innerHTML = '';
 
         for (todo of todos) {
-            var todoElement = document.createElement('li');
-            var todoText = document.createTextNode(todo);
+            const todoEl = document.createElement('li');
+            const todoText = document.createTextNode(todo);
 
-            todoElement.appendChild(todoText);
-            listEl.appendChild(todoElement);
+            const linkEl = document.createElement('a')
+            linkEl.setAttribute('href', '#');
+            linkEl.setAttribute('style', 'margin-left:15px;');
+
+            const pos = todos.indexOf(todo)
+
+            //linkEl.setAttribute('data-position', pos)
+            linkEl.setAttribute('onclick', 'deleteTodo('+pos+')') //not working, why ?! :S
+            const lintText = document.createTextNode('Excluir')
+
+            linkEl.appendChild(lintText)
+
+            todoEl.appendChild(todoText);
+            todoEl.appendChild(linkEl);
+
+            listEl.appendChild(todoEl);
         }
 }
 
 renderTodos();
 
+const addTodo = () => {
+    const todoText = inputEl.value;
 
-// const renderTodos = todos.map((el, inde) => {
-//     //console.log(el);
-// })
-
-const addTodo = () =>{
-    var todoText = inputEl.value;
+    if (!inputEl.value) {
+        infoEl.innerHTML = 'Prencha alguma informação'
+        inputEl.focus()
+        setTimeout(() => {
+            infoEl.innerHTML = ''
+        }, 4000)
+        return
+    }
 
     todos.push(todoText);
 
@@ -37,3 +60,9 @@ const addTodo = () =>{
 }
 
 btnEl.onclick = addTodo;
+
+
+const deleteTodo = pos => {
+    todos.splice(pos, 1)
+    renderTodos();
+}
